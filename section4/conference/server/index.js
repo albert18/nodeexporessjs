c0eb8b9e069708ed24cb3751cd844a8c5537f769
albert18 <caps18@hotmail.com>
2019-10-29 21:48:15 -0400

    update

--- section4/conference/server/index.js
+++ section4/conference/server/index.js
@@ -5,6 +5,11 @@ const routes = require('./routes');
 
 // if you want to use file call it static
 app.use(express.static('public'));
+
+app.get('/favicon.ico', (req,res,next) => {
+    return res.sendStatus(204);
+});
+
 app.use('/', routes());
 
 app.listen(3000);
